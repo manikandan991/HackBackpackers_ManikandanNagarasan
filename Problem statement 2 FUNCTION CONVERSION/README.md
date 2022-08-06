3dcc1d8cda346f4d6ac342e6735f273462e89c26
Manikandan.N <manikandan.n@8kmiles.local>
2022-08-06 14:59:29 +0530

    checkpoit 1

---
+++ Problem statement 2 FUNCTION CONVERSION/README.md
@@ -0,0 +1,14 @@
+# HackBackpackers_ManikandanNagarasan
+
+Problem Statements
+Problem Statement 2 - Function conversion
+# Description
+
+ADF to data stage fucntions conversion
+
+# Prerequisite
+python3 required
+
+# How to run
+
+Developing now
---
+++ Problem statement 2 FUNCTION CONVERSION/function_conversion.py
@@ -0,0 +1,37 @@
+def CurrentTimestamp_ADFTODataStage():
+    
+# Here ADF fucntion utcNow used
+
+#CurrentTimestamp
+# ADF == currentTimestamp() == toTimestamp('2250-12-31 12:12:12')
+    format = 'YYYY-MM-DD HH:MM:SS'
+    return utcNow(format)
+    
+def MinutesFromTime_ADFTODataStage(date_time):
+    
+# Here ADF fucntion split used , When passed date time 2250-12-31 12:12:12, second occurance of : value minutes used
+
+#CurrentTimestamp
+# ADF == currentTimestamp() == toTimestamp('2250-12-31 12:12:12')
+    minutes = split(':')[1]
+    return minutes
+    
+def DateFromDaysSince2_ADFTODataStage(date,num_of_days):
+
+# Here ADF fucntion addDays finding date by adding /subracting number of days
+    
+    
+# DateFromDaysSince2
+# Returns a date object by adding an integer to a baseline date. 
+# The integer can be negative to return a date that is earlier than the baseline date.
+
+# Input: number (int32), [baseline_date_object (date)]
+# Output: date (date)
+# Examples. If mylink.myintcol contains the integer 18250, and mylink.mydatecol contains the date 1958–08–18,
+# then the three following functions are equivalent, and return the date 2008–08–05:
+
+    return addDays(toDate(date), num_of_days)
+
+if __name__ == '__main__':
+    print('development in progress')
+    
